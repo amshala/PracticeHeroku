03c76005a5e35a38fcd670c540fe681ba73d9ea7
Amshala <ab5sz@virginia.edu>
2022-10-09 01:08:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Django Practice Assessment
+
+__Name:__ Russell Lee
+
+__Computing ID:__ rsl7ej
+
---
+++ polls/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.forms import ModelForm
+
+from .models import Choice, Question, Writing
+
+from django.http import HttpResponse
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+class DeepWriting(ModelForm):
+    class Meta:
+        model = Writing
+        fields = ['text']
+
+
+def DeepThoughtEntryView(request):
+    if request.method == "POST":
+        writing = DeepWriting(request.POST)
+        if writing.is_valid():
+            writing.save()
+        return HttpResponseRedirect(reverse('polls:listview'))
+    else:
+        writing = DeepWriting()
+    return render(request, 'polls/entryfield.html', {'writing': writing})
+
+
+def DeepThoughtListView(request):
+    template_name = 'polls/list.html'
+    return render(request, template_name, {'writings': Writing.objects.all(),
+                                           "num_writings": Writing.objects.count()})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
